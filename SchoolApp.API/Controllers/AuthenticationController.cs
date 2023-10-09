459cc4617aab06b1651540dac3f55c74dca12820
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2023-10-10 04:09:39 +0500

    Refreshing expired tokens

--- SchoolApp.API/Controllers/AuthenticationController.cs
+++ SchoolApp.API/Controllers/AuthenticationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using SchoolApp.API.Data;
@@ -74,13 +75,51 @@ namespace SchoolApp.API.Controllers
 
 			if (userExists != null && await _userManager.CheckPasswordAsync(userExists, loginVM.Password)) 
 			{
-				var tokenValue = await GenerateJWTTokenAsync(userExists);
+				var tokenValue = await GenerateJWTTokenAsync(userExists,null);
 				return Ok(tokenValue);
 			}
 			return Unauthorized();
 		}
 
-		private async Task<AuthResultVM> GenerateJWTTokenAsync(ApplicationUser user)
+
+		[HttpPost("refresh-token")]
+		public async Task<IActionResult> RefreshToken([FromBody] TokenRequestVM tokenRequestVM)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest("Please, provide all required fields");
+			}
+
+			var result = await VerifyAndGenerateTokenAsync(tokenRequestVM);
+			return Ok(result);
+		}
+
+		private async Task<AuthResultVM> VerifyAndGenerateTokenAsync(TokenRequestVM tokenRequestVM)
+		{
+			var jwtTokenHandler = new JwtSecurityTokenHandler();
+			var storedToken = await _context.RefreshTokens.FirstOrDefaultAsync(x => x.Token == tokenRequestVM.RefreshToken);
+			var dbUser = await _userManager.FindByIdAsync(storedToken.UserId);
+
+			try
+			{
+				var tokenCheckResult = jwtTokenHandler.ValidateToken(tokenRequestVM.Token, _tokenValidationParameters, out var validatedToken);
+
+				return await GenerateJWTTokenAsync(dbUser, storedToken);
+			}
+			catch (SecurityTokenExpiredException)
+			{
+				if (storedToken.DateExpire >= DateTime.UtcNow)
+				{
+					return await GenerateJWTTokenAsync(dbUser, storedToken);
+				}
+				else
+				{
+					return await GenerateJWTTokenAsync(dbUser, null);
+				}
+			}
+		}
+
+		private async Task<AuthResultVM> GenerateJWTTokenAsync(ApplicationUser user, RefreshToken rToken)
 		{
 			var authClaims = new List<Claim>()
 			{
@@ -90,6 +129,7 @@ namespace SchoolApp.API.Controllers
 				new Claim(JwtRegisteredClaimNames.Sub, user.Email),
 				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
 			};
+
 			var authSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["JWT:Secret"]));
 
 			var token = new JwtSecurityToken(
@@ -97,10 +137,20 @@ namespace SchoolApp.API.Controllers
 				audience: _configuration["JWT:Audience"],
 				expires: DateTime.UtcNow.AddMinutes(1),
 				claims: authClaims,
-				signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-				);
+				signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
 
-			var jwtToken=new JwtSecurityTokenHandler().WriteToken(token);
+			var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+			if (rToken != null)
+			{
+				var rTokenResponse = new AuthResultVM()
+				{
+					Token = jwtToken,
+					RefreshToken = rToken.Token,
+					ExpiresAt = token.ValidTo
+				};
+				return rTokenResponse;
+			}
 
 			var refreshToken = new RefreshToken()
 			{
@@ -109,18 +159,21 @@ namespace SchoolApp.API.Controllers
 				UserId = user.Id,
 				DateAdded = DateTime.UtcNow,
 				DateExpire = DateTime.UtcNow.AddMonths(6),
-				Token = Guid.NewGuid().ToString() + "_" + Guid.NewGuid().ToString()
+				Token = Guid.NewGuid().ToString() + "-" + Guid.NewGuid().ToString()
 			};
 			await _context.RefreshTokens.AddAsync(refreshToken);
 			await _context.SaveChangesAsync();
 
-			var response = new AuthResultVM() 
+
+			var response = new AuthResultVM()
 			{
 				Token = jwtToken,
 				RefreshToken = refreshToken.Token,
-				ExpiresAt =token.ValidTo
+				ExpiresAt = token.ValidTo
 			};
+
 			return response;
+
 		}
 	}
 }
